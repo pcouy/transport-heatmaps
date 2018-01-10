ccd542a61b534ea5dafb1a31a49dc7383dea8923
p1708143 <louismanhes@icloud.com>
2018-01-10 16:06:56 +0100

    calendar heatmap + données uber

---
+++ calendar/uber_merge.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pathlib
+
+currentDirectory = pathlib.Path('./data')
+currentPattern = "uber-raw-data-*"
+
+merged = []
+for currentFile in currentDirectory.glob(currentPattern): 
+    df = pd.read_csv(currentFile, sep=",", header=0, usecols=[0, 1, 2])
+    merged.append(df)
+
+result = pd.concat(merged)
+result.rename(columns={'Date/Time' : 'datetime'}, inplace=True)
+#result = result.sample(frac=0.0001)
+
+result.to_csv('data/uber_all_data.csv')
